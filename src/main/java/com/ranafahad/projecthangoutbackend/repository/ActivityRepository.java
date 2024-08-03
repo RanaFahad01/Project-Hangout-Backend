52a8102bc06e620abb582800eda4010735c8a895
Rana Fahad <ranafahadbh@gmail.com>
2024-08-03 21:17:25 +0200

    Fixed time_posted bug in ActivityRepository.java

--- src/main/java/com/ranafahad/projecthangoutbackend/repository/ActivityRepository.java
+++ src/main/java/com/ranafahad/projecthangoutbackend/repository/ActivityRepository.java
@@ -10,13 +10,13 @@ import java.util.List;
 public interface ActivityRepository extends CrudRepository<Activity, Long> {
 
     //The activities are sorted by descending time posted (earliest first)
-    @Query("SELECT * FROM activity ORDER BY timeposted DESC LIMIT 25")
+    @Query("SELECT * FROM activity ORDER BY time_posted DESC LIMIT 25")
     List<Activity> getAllActivities();
 
 
     //Returns all the activities posted by a specific user
     //Not used right now, but will be used when authentication is implemented
-    @Query("SELECT * FROM activity WHERE username = :username ORDER BY timeposted DESC")
+    @Query("SELECT * FROM activity WHERE username = :username ORDER BY time_posted DESC")
     List<Activity> getAllActivitiesByUser(String username);
 
 }
